4e597ce764620a0830b686252a2d9cd673f22b56
nguyenhoangloc2208 <nguyenhoangloc2208@gmail.com>
2023-10-02 14:07:46 +0700

    add header

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
+import Cart from './components/Cart';
+import Shop from './components/Shop';
+import User from './components/User';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+        <Router>
+          <NavBar/>
+          <Routes>
+            <Route path='/' element={<Home />} exact/>
+            <Route path='/shop' element={<Shop />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/user' element={<User />} />
+          </Routes>
+        </Router>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,55 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+const NavBar = (props) => {
+    return (<>
+        <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="/shop">Shop</Nav.Link>
+                    <Nav.Link href="/abouts">Abouts</Nav.Link>
+                    {/* <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">
+                        Another action
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">
+                        Separated link
+                    </NavDropdown.Item>
+                    </NavDropdown> */}
+                </Nav>
+                <Nav className='px-5'>
+                    <Form className="d-flex" style={{width: '400px'}}>
+                        <Form.Control
+                        type="search"
+                        placeholder="Search"
+                        className="me-2 w-100"
+                        aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                </Nav>
+                <Nav>
+                    <Nav.Link href="/cart">
+                    <span><i class="fa-solid fa-cart-shopping"/></span>
+                    </Nav.Link>
+                    <Nav.Link href="/user">
+                    <span><i class="fa-solid fa-user"/></span>
+                    </Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    </>)
+}
+
+export default NavBar;
