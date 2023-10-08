b795bd4ca627feb96d888916c9692ef5802b22b3
hoduy511 <hoduy244@gmail.com>
2023-10-08 21:16:43 +0700

    fix auth

---
+++ BackEnd/products/admin.py
@@ -0,0 +1,7 @@
+# Register your models here.
+from django.contrib import admin
+
+from products.models import Product, ProductCategory
+
+admin.site.register(ProductCategory)
+admin.site.register(Product)
