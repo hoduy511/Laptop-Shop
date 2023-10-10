add0438b6ad2e5b2c9342d0734f3925cb4675392
hoduy511 <hoduy244@gmail.com>
2023-10-10 16:24:39 +0700

    add app users

--- BackEnd/core/urls.py
+++ BackEnd/core/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/user/", include("users.urls", namespace="users")),
     path('api/products/', include('products.urls', namespace='products')),
 ]
