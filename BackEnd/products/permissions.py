9c7215482c05f82001ff2ceed142022dfdadbf68
hoduy511 <hoduy244@gmail.com>
2023-10-17 13:15:42 +0700

    add permissons

--- BackEnd/products/permissions.py
+++ BackEnd/products/permissions.py
@@ -1,56 +1,16 @@
-from django.contrib.auth import get_user_model
-from django.db import models
-from django.utils.translation import gettext_lazy as _
-
-User = get_user_model()
+from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
-def category_image_path(instance, filename):
-    return f"product/category/icons/{instance.name}/{filename}"
+class IsSellerOrAdmin(BasePermission):
+    """
+    Check if authenticated user is seller of the product or admin
+    """
 
+    def has_permission(self, request, view):
+        return request.user.is_authenticated is True
 
-def product_image_path(instance, filename):
-    return f"product/images/{instance.name}/{filename}"
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
 
-
-class ProductCategory(models.Model):
-    name = models.CharField(_("Category name"), max_length=100)
-    icon = models.ImageField(upload_to=category_image_path, blank=True)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        verbose_name = _("Product Category")
-        verbose_name_plural = _("Product Categories")
-
-    def __str__(self):
-        return self.name
-
-
-def get_default_product_category():
-    return ProductCategory.objects.get_or_create(name="Others")[0]
-
-
-class Product(models.Model):
-    seller = models.ForeignKey(
-        User, related_name="products", on_delete=models.CASCADE)
-    category = models.ForeignKey(
-        ProductCategory,
-        related_name="product_list",
-        on_delete=models.SET(get_default_product_category),
-    )
-    name = models.CharField(max_length=200)
-    desc = models.TextField(_("Description"), blank=True)
-    image = models.ImageField(upload_to=product_image_path, blank=True)
-    price = models.DecimalField(decimal_places=2, max_digits=10)
-    quantity = models.IntegerField(default=1)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ("-created_at",)
-
-    def __str__(self):
-        return self.name
+        return obj.seller == request.user or request.user.is_admin
